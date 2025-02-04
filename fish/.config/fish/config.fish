f94658e0f245ad215b2efc2930a4ebf2b697a1be
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-02-04 12:25:22 -0300

    add asdf fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,6 +17,20 @@ set -x ANDROID_HOME $HOME/Android/Sdk
 set -x PATH $PATH $ANDROID_HOME/emulator
 set -x PATH $PATH $ANDROID_HOME/platform-tools
 
+ # ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 # PNPM
 set -gx PNPM_HOME "/home/pedro-neto/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
