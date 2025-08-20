ebca001400a01c0ffe8bb4796aa448a1cb8c94d3
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-08-19 22:24:09 -0300

    fish modules

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,45 +1,10 @@
 # Configurações globais
 # NVIM
 set PATH $PATH /opt/nvim-linux-x86_64/bin
-alias v 'nvim'
+alias v nvim
 # NVIM END
 
-# Cargo (Rust)
-set -x PATH $HOME/.cargo/bin $PATH
-
-# Golang
-set -x PATH $PATH /usr/local/go/bin
 set -x GOPATH $HOME/go
-set -x PATH $PATH $GOROOT/bin $GOPATH/bin
-
-# Android SDK
-set -x ANDROID_HOME $HOME/Android/Sdk
-set -x PATH $PATH $ANDROID_HOME/emulator
-set -x PATH $PATH $ANDROID_HOME/platform-tools
-
-# intellij-idea
-set -x PATH $PATH $HOME/intellij-idea/bin
-#
-
- # ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-
-# PNPM
-set -gx PNPM_HOME "/home/pedro-neto/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
 
 if status is-interactive
     # Configurações para sessões interativas
@@ -58,7 +23,7 @@ if status is-interactive
       --preview 'bat -n --color=always {}'
       --border
       --bind 'ctrl-/:change-preview-window(down|hidden|)'"
-    
+
     # Configuração para copiar para o clipboard com CTRL-Y
     export FZF_CTRL_R_OPTS="
       --color header:italic
@@ -73,4 +38,10 @@ if status is-interactive
 
     # Inicializar Starship para prompt moderno
     starship init fish | source
+end
+
+set -l module_dir $HOME/.config/fish/modules
+
+if test -n "$CONTAINER_ID" -a -f "$module_dir/container.fish"
+    source $module_dir/container.fish
 end
