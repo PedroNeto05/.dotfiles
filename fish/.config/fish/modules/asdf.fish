7e09b672f42a19123382b0826d1fa9757607c445
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-09-15 22:37:56 -0300

    change asdf to normal linux

--- fish/.config/fish/modules/asdf.fish
+++ fish/.config/fish/modules/asdf.fish
@@ -1,16 +1,10 @@
-if test -n "$CONTAINER_ID" -a -f "$module_dir/asdf.fish"
-    if test -f $HOME/.asdf/asdf.fish
-        source $HOME/.asdf/asdf.fish
-    end
-
-    if test -z "$ASDF_DATA_DIR"
-        set _asdf_shims "$HOME/.asdf/shims"
-    else
-        set _asdf_shims "$ASDF_DATA_DIR/shims"
-    end
-
-    if not contains $_asdf_shims $PATH
-        set -gx --prepend PATH $_asdf_shims
-    end
-    set --erase _asdf_shims
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
+
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
