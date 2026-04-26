3847eb5941fd736612dfab5769c2aa71a56fa9e6
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2026-04-26 12:13:03 -0300

    stow

--- stow.sh
+++ stow.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
+STOW_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PACKAGES=(fish kitty starship tmux)
 
 for pkg in "${PACKAGES[@]}"; do
-    if [ -d "$pkg" ]; then
-        echo "→ Linkando: $pkg"
-        stow -R "$pkg" -t "$HOME"
-    fi
+  if [ -d "$STOW_DIR/$pkg" ]; then
+    echo "Linkando: $pkg"
+    stow -d "$STOW_DIR" -R "$pkg" -t "$HOME"
+  fi
 done
